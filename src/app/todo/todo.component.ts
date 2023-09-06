d005dc4e77acd4b705540dc8afc652fdf5cfbed5
EjnarRaidriar <emanuil.cucos25@gmail.com>
2023-09-06 11:57:43 +0300

    filter parameters are taken from the URL whenever it changes, deleted filtering implementation

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,9 +1,13 @@
-import { Component, Signal, WritableSignal, computed, inject, signal } from '@angular/core';
+import { Component, Signal, computed, effect, inject } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
-import { TaskService } from '../task.service';
-import { Todo } from '../todo.interface';
 
+import { TaskService } from '../task.service';
+
+import { Subscription } from 'rxjs';
+
+import { Todo } from '../todo.interface';
+import { FilterParams } from '../filterParams.interface';
 
 @Component({
   selector: 'app-todo',
@@ -15,22 +19,34 @@ export class TodoComponent {
     selectedTaskId: Signal<string | null> = computed(() => {
         return this.taskService.selectedTaskId()});
 
-    todoList: Signal<Todo[]> = computed(() => {
+    taskList: Signal<Todo[]> = computed(() => {
         return this.taskService.taskList()});
-    
-    filteredList: Signal<Todo[]> = computed(() => {
-        return this.filterList();
-    })
 
-    searchInput: WritableSignal<string> = signal('');
+    filteredTaskList: Signal<Todo[]> = computed(() => {
+        return this.filterTasks();
+    });
 
-    filterOption: WritableSignal<string> = signal('all');
+    filter: FilterParams = {
+        completion: 'all',
+        orderByDate: 'default',
+        search: ''
+    };
 
     route = inject(ActivatedRoute);
     taskService = inject(TaskService);
+
+    subscribtion: Subscription | undefined = undefined;
     
     ngOnInit(): void {
         this.taskService.loadData();
+        this.subscribtion = this.route.queryParams.subscribe((params) => {
+            this.filter = {
+                completion: params['completion'] || 'all',
+                orderByDate: params['orderByDate'] || 'default',
+                search: params['search'] || ''
+            }
+            console.log(this.filter);
+        })
     }
 
     changeCompletion(id: string) {
@@ -40,46 +56,6 @@ export class TodoComponent {
     selectTask(id: string) {
         this.taskService.selectTask(id);
     }
-
-    setSearchString(e: Event) {
-        this.searchInput.set((e.target as HTMLInputElement).value);
-    }
-
-    setFilterOption(e: Event) {
-        this.filterOption.set((e.target as HTMLInputElement).value);
-    }
-
-    filterList(): Todo[] {
-        // filtering by search
-        let filteredList = this.todoList().filter(task => task.title.toLowerCase().includes(this.searchInput()));
-        // filtering by completion
-        let completedList = filteredList.filter(task => task.isCompleted);
-        let activeList = filteredList.filter(task => !task.isCompleted);
-        // filtering by filter option
-        if (this.filterOption() === 'completed') return completedList;
-        if (this.filterOption() === 'active') return activeList;
-        if (this.filterOption() === 'by date') {
-            let undefinedCompletedList = completedList.filter(task => task.completeBefore === undefined);
-            let undefinedActiveList = activeList.filter(task => task.completeBefore === undefined);
-            completedList = completedList.filter(task => task.completeBefore !== undefined);
-            activeList = activeList.filter(task => task.completeBefore !== undefined);
-            completedList.sort((a, b) => {
-                return a.completeBefore! > b.completeBefore! ? 1 : -1;
-            });
-            activeList.sort((a, b) => {
-                return a.completeBefore! > b.completeBefore! ? 1 : -1;
-            });
-            completedList = completedList.concat(undefinedCompletedList);
-            activeList = activeList.concat(undefinedActiveList);
-        }
-        filteredList = activeList.concat(completedList);
-        return filteredList;
-    }
-
-    applyFilter(filter: string) {
-        console.log(filter);
-    }
-
 
     //For CSS
     listClass(id: string) {
@@ -93,5 +69,10 @@ export class TodoComponent {
         } else {
             return 'active-item';
         }
+    }
+
+    filterTasks(): Todo[] {
+
+        return this.taskList();
     }
 }
