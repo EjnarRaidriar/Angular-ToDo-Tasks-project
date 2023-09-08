ae46788fcf909a02dda13535edad5e958c5ca55e
EjnarRaidriar <emanuil.cucos25@gmail.com>
2023-09-08 16:31:35 +0300

    added aditional interfaces for filter params

--- src/app/filterParams.interface.ts
+++ src/app/filterParams.interface.ts
@@ -1,3 +1,11 @@
+export interface Completion {
+    value: "all" | "active" | "completed";
+}
+
+export interface OrderByDate {
+    value: "default" | "asc" | "desc";
+}
+
 export interface FilterParams {
     completion: "all" | "active" | "completed";
     orderByDate: "default" | "asc" | "desc";
